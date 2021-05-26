af03e91d133d2d925009ec0284fec171a94ac7aa
Chanchal-S <41203033+Chanchal-S@users.noreply.github.com>
2021-05-27 02:15:41 +0530

    Commit id-02 - EventAdmin class to admin

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Event
 
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('user_name','event_desc','event_date','event_time')
+    ordering = ('user_name',)
+    search_fields = ('user_name',)
+
+admin.site.register(Event, EventAdmin)
 
 # Register your models here.
