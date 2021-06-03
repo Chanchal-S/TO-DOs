d2f5d37f8bff6557ce255c669497a210ed0b1ab0
Chanchal-S <41203033+Chanchal-S@users.noreply.github.com>
2021-06-03 11:43:22 +0530

    changes to UI, displays database content

--- events/models.py
+++ events/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from datetime import date,datetime
 
 class Event(models.Model):
-    user_name = models.CharField(default='username',max_length=100, primary_key=True)
+    user_name = models.CharField(default='username',max_length=100,primary_key=True)
     event_desc = models.TextField(max_length=200)
     event_date = models.DateField(default=date.today())
     event_time = models.TimeField(default=datetime.time(datetime.now()))
--- events/views.py
+++ events/views.py
@@ -1,8 +1,20 @@
 from django.http.request import host_validation_re
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Event
 
 def index(request):
-    return HttpResponse("<h1>Reminder App</h1>")
+    #return HttpResponse("<h1>Reminder App</h1>")
+    return render(request, 
+            'events/event.html'
+            )
+
+##def all_events(request):
+    event_list = Event.objects.all()
+    return render(request,
+        'events/event_list.html',
+        {'event_list':event_list}
+        )
+###
 
 # Create your views here.
